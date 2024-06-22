a2427c11b86e1381483c2a93113ba4f0604f8297
Jay Lhomme <jhgatohagire@gmail.com>
2024-06-22 08:57:20 -0500

    prepping PostTags for Post

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -2,3 +2,5 @@ from .RareUserView import RareUserView
 from .auth import register_user, check_user
 from .post import PostView
 from .category import CategoryView
+from .tags import TagView, TagSerializer
+from .post_tags import PostTagView, PostTagSerializer
