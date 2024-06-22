5ce8093df408d7274e7e869209cf384bca520517
unknown <hautersolomon@gmail.com>
2024-06-22 09:14:49 -0500

    full crud categories

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -1,29 +1,37 @@
-from rest_framework import  status, serializers
+from rest_framework import status, serializers
 from rest_framework.response import Response
 from rareapi.models import Category
 from rest_framework.viewsets import ViewSet
 
-
-
 class CategoryView(ViewSet):
     
-    def list(self,request):
+    def list(self, request):
         category = Category.objects.all()
-        serializer = CategorySerializer(category, many= True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-    def retrieve(self,request,pk):
-         category = Category.objects.get(pk = pk)
-         serializer = CategorySerializer(category)
-         return Response(serializer.data, status=status.HTTP_200_OK)
-    def update(self,request,pk):
-        category = Category.objects.get(pk = pk)
-        category.label = request.data.get("label")
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk):
+        category = Category.objects.get(pk=pk)
         serializer = CategorySerializer(category, data=request.data)
         if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-    def destroy(self,request, pk):
-        category = Category.objects.get(pk = pk)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk):
+        category = Category.objects.get(pk=pk)
         category.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
         
